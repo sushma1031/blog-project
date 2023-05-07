2b7ef28cf74239b5ecde73a69188939df3f7232e
sushma1031 <sushmajram@gmail.com>
2023-05-07 15:22:26 +0530

    Display all posts in newest first order

--- controllers/getAllPosts.js
+++ controllers/getAllPosts.js
@@ -3,6 +3,7 @@ const date = require("../date.js");
 
 module.exports = (req, res) => {
   Post.find({})
+    .sort({ createdAt: -1 })
     .then((posts) => {
       posts.forEach((post) => {
         post.dateString = date.getDate(post.createdAt);
