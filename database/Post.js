78fe177e526423f711e852d8076e6dcb9028764b
sushma1031 <sushmajram@gmail.com>
2023-05-07 14:04:34 +0530

    Format posts, navbar
    - Format all posts posts page
    - Remove border for navbar toggler button
    - Update Post model to have empty string as default for post content

--- database/Post.js
+++ database/Post.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const postSchema = new mongoose.Schema({
   title: String,
-  content: String,
+  content: { type: String, default: "" },
   username: String,
   preview: { type: String, default: "" },
   image: {
