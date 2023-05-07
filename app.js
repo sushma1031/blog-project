69e8343e61832677af38d320be752462ae755f9d
sushma1031 <sushmajram@gmail.com>
2023-05-07 18:38:59 +0530

    Update home, about and contact pages
    Also update case of "enctype" in compose

--- app.js
+++ app.js
@@ -22,14 +22,6 @@ const redirectIfAuthenticated = require("./controllers/middleware.js").redirect;
 const auth = require("./controllers/middleware.js").auth;
 const deletePostController = require("./controllers/deletePost.js");
 
-const Post = require("./database/Post.js");
-
-
-const aboutContent =
-  "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent =
-  "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-
 const app = express();
 
 app.set("view engine", "ejs");
@@ -82,11 +74,11 @@ app.use(function (req, res, next) {
 app.get("/", homePageController);
 
 app.get("/about", (req, res) => {
-  res.render("about", { content: aboutContent });
+  res.render("about");
 });
 
 app.get("/contact", (req, res) => {
-  res.render("contact", { content: contactContent });
+  res.render("contact");
 });
 
 app.get("/posts", getAllPostsController);
--- controllers/homePage.js
+++ controllers/homePage.js
@@ -1,9 +1,6 @@
 const Post = require("../database/Post.js");
 const date = require("../date.js");
 
-const homeStartingContent =
-  "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Cursis molestie a iaculis at erat pellentesque adipiscing.";
-
 module.exports = (req, res) => {
   Post.find({})
     .sort({ createdAt: -1 })
@@ -13,7 +10,6 @@ module.exports = (req, res) => {
         post.relativeDate = date.calcDate(post.createdAt);
       });
       res.render("home", {
-        startingContent: homeStartingContent,
         posts: posts,
       });
     })
