f495607ff7037b0ecb474c7b9d83bf8e6f7cce80
sushma1031 <sushmajram@gmail.com>
2023-05-08 14:01:10 +0530

    Update user registration
    Ensure that sessions are not created for failed registrations

--- controllers/storeUser.js
+++ controllers/storeUser.js
@@ -2,20 +2,21 @@ const User = require("../database/User.js");
 
 module.exports = (req, res) => {
   if (req.body.email !== process.env.EMAIL) {
-      const arguments = {
-        statusCode: "Sorry :(",
-        message:
-          "You do not have permission to create an account. Please contact the owner of this blog.",
-        redirect: "/contact",
-        button: "Contact",
-      };
-      return res.render("error", arguments);
-  }
+    const arguments = {
+      statusCode: "Sorry :(",
+      message:
+        "You do not have permission to create an account. Please contact the owner of this blog.",
+      redirect: "/contact",
+      button: "Contact",
+    };
+    return res.render("error", arguments);
+  } else {
 
-  User.create(req.body)
-    .then((registeredUser) => {
-      req.session.userId = registeredUser._id;
-      res.redirect("/");
-    })
-    .catch((err) => console.log(err));
+    User.create(req.body)
+      .then((registeredUser) => {
+        req.session.userId = registeredUser._id;
+        res.redirect("/");
+      })
+      .catch((err) => console.log(err));
+  }
 };
