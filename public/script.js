ef8e15bce8089e06fd8824d4b5857159ca4ca8c6
sushma1031 <sushmajram@gmail.com>
2023-06-07 21:41:50 +0530

    Update script.js
    Disable inputs when modal is displayed

--- public/script.js
+++ public/script.js
@@ -1,7 +1,10 @@
 let openBtn = document.getElementById("open-modal");
 let closeBtn = document.getElementById("close-modal");
+let form = document.querySelector(".contact-form");
 let content = document.querySelector(".content-wrap");
-let modalDiv = content.querySelector("#modal");
+let modalDiv = document.querySelector("#modal");
+let inputs = form.querySelectorAll("input");
+let textarea = form.querySelector("textarea");
 
 openBtn.addEventListener("click", displayModal);
 
@@ -10,6 +13,10 @@ closeBtn.addEventListener("click", closeModal);
 function displayModal() {
   content.classList.add("modal-open");
   openBtn.setAttribute("disabled", true);
+  inputs.forEach(input => {
+    input.setAttribute("disabled", true);
+  })
+  textarea.setAttribute("disabled", true);
   modalDiv.style.display = "flex";
   window.scrollTo(0, 0);
 }
@@ -17,5 +24,9 @@ function displayModal() {
 function closeModal() {
   content.classList.remove("modal-open");
   openBtn.removeAttribute("disabled");
+  inputs.forEach((input) => {
+    input.removeAttribute("disabled");
+  });
+  textarea.removeAttribute("disabled");
   modalDiv.style.display = "none";
 }
