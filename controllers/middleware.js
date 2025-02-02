64e5b6d0ab9e881bc9056f2390f43413b5665dee
sushma1031 <sushmajram@gmail.com>
2025-02-02 12:35:57 +0530

    Refactor auth middleware

--- controllers/middleware.js
+++ controllers/middleware.js
@@ -1,27 +1,28 @@
 const User = require("../database/User");
 
 const redirect = (req, res, next) => {
-  if (req.session.userId) {
+  if (req.session && req.session.userId) {
     return res.redirect("/");
-  }
-  else
-    next();
+  } else next();
 };
 
 const auth = (req, res, next) => {
-User.findById(req.session.userId)
-      .then((user) => {
-        if (!user) {
+  if (!req.session || !req.session.userId)
+    return res.redirect("/login");
+
+  User.findById(req.session.userId)
+        .then((user) => {
+          if (!user) {
+            return res.redirect("/login");
+          } else {
+            res.locals.username = user.username;
+            return next();
+          }
+        })
+        .catch((error) => {
+          console.log(error.message);
           return res.redirect("/login");
-        } else {
-          res.locals.username = user.username;
-          return next();
-        }
-      })
-      .catch((error) => {
-        console.log(error.message);
-        return res.redirect("/login");
-      });
+        });
 };
 
 module.exports = {redirect, auth}
